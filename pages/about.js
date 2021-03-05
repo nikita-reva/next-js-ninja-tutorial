89bb84c5c2a6623e6b0c683ba1f976e29603f7b7
nikita-reva <68555738+nikita-reva@users.noreply.github.com>
2021-03-05 14:01:22 +0100

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+
+const About = () => {
+	return (
+		<>
+			<Head>
+				<title>Ninja List | About</title>
+				<meta name="keywords" content="ninjas" />
+				<meta name="description" content="Ninja List" />
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+					Suscipit dolorem laboriosam voluptas sed temporibus
+					consequuntur quos perspiciatis numquam voluptate ipsum?
+				</p>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+					Suscipit dolorem laboriosam voluptas sed temporibus
+					consequuntur quos perspiciatis numquam voluptate ipsum?
+				</p>
+			</div>
+		</>
+	)
+}
+
+export default About
